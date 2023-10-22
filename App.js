5a961b65bfc6c8a70f6cca064b3ea8616886374a
ceejay900 <58793044+ceejay900@users.noreply.github.com>
2023-10-22 13:42:46 +0800

    modified

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+// every time ngrok executed make sure to change the fetch "value" besides "PATH(/server)" to match the nrok generated link.
+// open the link click "Visit Site" for authentication,
+// otherwise it won't connect to back-end.
+import { StyleSheet, Text, TouchableOpacity, View, Image, Modal } from 'react-native';
 import { Camera } from 'expo-camera';
 import React, { useState, useRef, useEffect } from 'react';
 
 
-export default function App() {
 
+export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.front);
   const cameraRef = useRef(null);
-  const [mydata, setMyData] = useState([]);
+  const [timer, setTimer] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [mydata, setMyData] = useState('');
 
 
 
   useEffect(() => {
+    // check permission 
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === 'granted');
@@ -27,55 +34,151 @@ export default function App() {
     return <Text>No access to camera</Text>
   }
 
+  //
+
+  const TimerDefault = () => {
+    if (!timer) {
+      setTimer(5);
+
+      const timerInterval = setInterval(() => {
+        setTimer(prevTimer => prevTimer - 1);
+      }, 1000);
+
+      setTimeout(async () => {
+        clearInterval(timerInterval);
+        TakePicture();
+      }, 5000)
+    }
+
+  }
+
   const TakePicture = async () => {
     if (cameraRef.current) {
       try {
-        const options = { quality: 0.5, base64: true };
+        const options = { quality: 0.5, base64: true, };
         const data = await cameraRef.current.takePictureAsync(options);
 
-        // console.log('Photo save at? ', data.uri);
-        // console.log(data.base64);
-
-        fetch('https://be36-2001-4450-46b7-f00-c138-7e62-20e5-b9c0.ngrok-free.app/server', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ image: data.base64 })
-        })
-          .then(response => response.json())
-          .then(data => {
-            console.log(data);
-            setMyData(data);
+        fetch('https://curvz-style-client-mobile.onrender.com/server',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ image: data.base64 })
+          })
+          .then(async (response) => {
+            const contentType = response.headers.get('content-type');
+            if (contentType && contentType.includes('application/json')) {
+              const data = await response.json();
+              console.log(data);
+              setMyData(data);
+            } else {
+              console.error('Response is not JSON');
+            }
           })
           .catch(e => console.error("Error: ", e))
+          .finally(() => {
+            setTimer(null);
+          });
       }
       catch (error) {
         console.error(error);
+        setTimer(null);
       }
     }
   }
+
+  const BulletText = ({ text }) => (
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30, marginRight: 5 }}>•</Text>
+      <Text style={{ fontSize: 20 }}>{text}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
+
+      {/* cameraView */}
       <Camera style={{ flex: 1, }} type={type} ref={cameraRef}>
         <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row', alignSelf: 'center' }}>
           <View style={styles.rectangle}></View>
         </View>
+
+        {/* how to use floating button*/}
+        <TouchableOpacity style={{ height: 50, width: 50, zIndex: 2, position: 'absolute', left: 300, top: 30 }}
+          onPress={() => setModalVisible(true)}>
+          <Image style={{ width: 50, height: 50 }} source={require('./resources/how.png')} />
+        </TouchableOpacity>
+        {/* customize alert Dialog  */}
+        <Modal
+          animationType='slide'
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => { setModalVisible(false) }}
+        >
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ backgroundColor: 'grey', padding: 20, borderRadius: 10, alignItems: 'center' }}>
+              {/* Add your modal content here */}
+              <Text style={{ fontWeight: 'bold' }}>HOW TO USE?</Text>
+              <Text>Take a picture with a 5-seconds default timer.</Text>
+              <Image style={{ width: 280, height: 230, borderRadius: 10, margin: 10 }} source={require('./resources/ref.png')} />
+              <Text style={{ fontWeight: 'bold' }}>IDEAL POSITION:</Text>
+              <View>
+                <BulletText text='Pitch angle - Portrait shot' />
+                <BulletText text='Height from ground - 20cm' />
+                <BulletText text='Distance from character - 50cm' />
+                <BulletText text='Focal angle - Reference reactangle' />
+              </View>
+              <TouchableOpacity style={{ marginTop: 10, width: 50, height: 30, justifyContent: 'center', alignItems: 'center' }} onPress={() => setModalVisible(false)}>
+                <Text style={{ fontWeight: 'bold', color: 'red', fontSize: 20 }}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+
+        {/* Flip button  */}
+        <TouchableOpacity style={{
+          borderRadius: 100, backgroundColor: 'grey', width: 50, height: 50, justifyContent: 'center', position: 'absolute',
+          top: 100, left: 300
+        }}
+          onPress={() => setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          )}>
+          <Text style={{ fontSize: 20, alignSelf: 'center', color: 'white', fontWeight: 'bold' }}> F </Text>
+        </TouchableOpacity>
+
       </Camera>
-      <TouchableOpacity style={{ flex: 0.1, alignSelf: 'flex-end', alignItems: 'center' }}
-        onPress={() => setType(
-          type === Camera.Constants.Type.back
-            ? Camera.Constants.Type.front
-            : Camera.Constants.Type.back
-        )}>
-        <Text style={{ fontSize: 18, marginBottom: 10, color: 'red' }}> Flip </Text>
-      </TouchableOpacity>
-      {/* <Text style={{ fontSize: 18, marginBottom: 10, color: 'red' }}> {mydata.shoulder_width_cm} </Text> */}
-      <TouchableOpacity style={{ alignSelf: 'flex-end', alignItems: 'center', marginBottom: 20 }}
-        onPress={TakePicture}>
-        <Text style={{ fontSize: 18, marginBottom: 10, color: 'red' }}> TakePicture </Text>
-      </TouchableOpacity>
-    </View>
+
+      {/* buttons  */}
+      <View style={{ height: '40%', width: '100%', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
+        {/* display timer count */}
+        {timer !== null && (
+          <Text style={{ fontSize: 18, color: 'blue' }}>{timer} seconds remaining</Text>
+        )}
+
+        {/* TakePicture button  */}
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity style={styles.button}
+            onPress={TakePicture}>
+            <Text style={styles.buttonText}> TakePicture </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}
+            onPress={TimerDefault}>
+            <Text style={styles.buttonText}> 5sTimer </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: 'column' }}>
+          <BulletText text={'Shoulder: ' + mydata.shoulder_width_cm}></BulletText>
+          <BulletText text={'Top: ' + mydata.top_length_cm}></BulletText>
+          <BulletText text={'Leg: ' + mydata.leg_length_cm}></BulletText>
+        </View>
+
+
+      </View>
+    </View >
   );
 }
 
@@ -84,7 +187,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   rectangle: {
-    borderColor: 'black',
+    borderColor: 'white',
     borderWidth: 5,
     alignSelf: 'center',
     zIndex: 1,
@@ -92,5 +195,19 @@ const styles = StyleSheet.create({
     width: '50%',
     marginBottom: 20
   },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+    margin: 10,
+    height: 50,
+    width: '30%',
+    borderRadius: 10
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  }
 
 });
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ReactNative
 ngrok is a widely used networking tool that creates secure tunnels to localhost, allowing you to expose a local server to the internet.
 to run and used!
+terminal 1:
 -npm start
+terminal 2:
+-./myenv/Scripts/activate
 -python app.py
+terminal 3:
 -./ngrok http 5000
